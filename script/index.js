30a7a01abfdf11c53e70b3ff90fb4630627ebbab
폴리나 <elfish33@gmail.com>
2025-11-10 18:09:41 +0900

    New Balance Responsive

--- script/index.js
+++ script/index.js
@@ -5,20 +5,52 @@ const mainBnrSwiper =  new Swiper ('#main_bnr_swiper', {
 });
 
 const shopByAgeSwiper = new Swiper ('#age_swiper',{
-    slidesPerView: 2.5,
-    spaceBetween: 15,
+    //mobile 기준
+    slidesPerView: 1.3,
+    spaceBetween: 40,
+    breakpoints: {
+        640: {
+            slidesPerView: 1.5,
+            spaceBetween: 15,
+        },
+        1024 : {
+            slidesPerView: 2.5,
+            spaceBetween: 15,
+        },
+    },
 })
 
 const trendSwiper = new Swiper ('.trend_swiper_wrap #trend_swiper', {
-    slidesPerView: 4.3,
+    slidesPerView: 1.5,
     spaceBetween: 20,
     scrollbar: {
         el: '.trend_swiper_wrap .swiper-scrollbar',
         hide: false,
-    }
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2.3,
+            spaceBetween: 20,
+        },
+        1020: {
+            slidesPerView: 3.3,
+            spaceBetween: 20,
+        },
+    },
 })
 
 const styleSwiper = new Swiper ('#stylestory_swiper',{
-    slidesPerView: 3.1,
-    spaceBetween: 20,
+    slidesPerView: 1.5,
+    spaceBetween: 15,
+    loop: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 2.5,
+            spaceBetween: 15,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+    },
 })
